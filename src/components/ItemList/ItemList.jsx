143d648c077e2662249c27b4b7c3b69b31709c1c
Maximiliano Ratti <telcom.web@gmail.com>
2025-10-27 11:44:54 -0300

    Actualización del css de las cards

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,26 +1,20 @@
 import { Link } from "react-router-dom";
 import { Item } from "../Item/Item";
+import "./ItemList.css";
 
 export const ItemList = ({ list }) => {
-  //pasamos el button como children, no es obligatorio
-
   return (
-    <>
+    <div className="product-list">
       {list.length ? (
         list.map((prod) => (
-          //como yo reutilizo Item en el detalle, no quiero conflictos de "click"
-          //al tocar el boton de "agregar al carrito" y se clickee tambien la card
-
-          //Por eso para evitar poner Link en Item, venimos a ItemList y pasamos
-          //Link en el mapeo, con su key, ya que la key va en el componente que retorne el mapeo
-          //(y ahora no es Item, sino Link)
-          <Link to={`/detail/${prod.id}`} key={prod.id}>
+          <Link to={`/detail/${prod.id}`} key={prod.id} className="product-link">
             <Item {...prod} />
           </Link>
         ))
       ) : (
         <p>No hay productos</p>
       )}
-    </>
+    </div>
   );
 };
+
